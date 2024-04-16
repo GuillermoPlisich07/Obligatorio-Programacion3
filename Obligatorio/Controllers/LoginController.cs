4697dac108206e9596998147c3374114a71ad1fa
DiegoCas04 <diegocastillo2301@gmail.com>
2024-04-16 20:02:44 -0300

    CreeLosViewModelYControladorLogin

---
+++ Obligatorio/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using LogicaDatos.Repositorios;
+using LogicaNegocio.Dominio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Obligatorio.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly LibreriaContext ctx;
+
+        public LoginController(LibreriaContext context)
+        {
+            ctx = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string nombreUsuario, string contraseña)
+        {
+            var usuario = ctx.Usuarios.FirstOrDefault(u => u.nombre == nombreUsuario && u.password == contraseña);
+
+            if (usuario != null)
+            {
+                // Usuario autenticado, redirige a la página principal
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Autenticación fallida, muestra un mensaje de error
+                ModelState.AddModelError(string.Empty, "Nombre de usuario o contraseña incorrectos.");
+                return View();
+            }
+        }
+    }
+}
--- Obligatorio/Models/UsuarioViewModels/ModificarUsuarioViewModel.cs
+++ Obligatorio/Models/UsuarioViewModels/ModificarUsuarioViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace Obligatorio.Models.Usuario
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Obligatorio.Models.Usuario
 {
     public class ModificarUsuarioViewModel
     {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [RegularExpression(@"^[a-zA-Z][-a-zA-Z' ]*[a-zA-Z]$", ErrorMessage = "El nombre no tiene un formato válido")]
+        [MaxLength(50)] // Cambiar según la longitud máxima permitida en la base de datos
+        [Column(TypeName = "nvarchar(50)")] // Cambiar según el tipo de datos de la columna en la base de datos
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [RegularExpression(@"^[a-zA-Z][-a-zA-Z' ]*[a-zA-Z]$", ErrorMessage = "El apellido no tiene un formato válido")]
+        [MaxLength(50)] // Cambiar según la longitud máxima permitida en la base de datos
+        [Column(TypeName = "nvarchar(50)")] // Cambiar según el tipo de datos de la columna en la base de datos
+        public string apellido { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.,;¡!])[A-Za-z\d.,;¡!]+$", ErrorMessage = "La contraseña no cumple con los requisitos mínimos")]
+        [NotMapped] // No mapear este campo a la base de datos
+        public string password { get; set; }
     }
 }
